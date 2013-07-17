59b95f45cb1d4980c40f70b4f82c171bfad48f35
Vladimir Grigor <vgrigor@gmail.com>
2013-07-17 12:01:34 +0300

    modified folds output format to be more csv convenient

--- nltk_trainer/classification/scoring.py
+++ nltk_trainer/classification/scoring.py
@@ -6,22 +6,22 @@ from nltk.probability import FreqDist, ConditionalFreqDist
 def sum_category_word_scores(categorized_words, score_fn):
 	word_fd = FreqDist()
 	category_word_fd = ConditionalFreqDist()
-	
+
 	for category, words in categorized_words:
 		for word in words:
 			word_fd.inc(word)
 			category_word_fd[category].inc(word)
-	
+
 	scores = collections.defaultdict(int)
 	n_xx = category_word_fd.N()
-	
+
 	for category in category_word_fd.conditions():
 		n_xi = category_word_fd[category].N()
-		
+
 		for word, n_ii in category_word_fd[category].iteritems():
 			n_ix = word_fd[word]
 			scores[word] += score_fn(n_ii, (n_ix, n_xi), n_xx)
-	
+
 	return scores
 
 def sorted_word_scores(wsdict):
@@ -30,33 +30,33 @@ def sorted_word_scores(wsdict):
 def ref_test_sets(classifier, test_feats):
 	refsets = collections.defaultdict(set)
 	testsets = collections.defaultdict(set)
-	
+
 	for i, (feat, label) in enumerate(test_feats):
 		refsets[label].add(i)
 		observed = classifier.classify(feat)
 		testsets[observed].add(i)
-	
+
 	return refsets, testsets
 
 def multi_ref_test_sets(multi_classifier, multi_label_feats):
 	refsets = collections.defaultdict(set)
 	testsets = collections.defaultdict(set)
-	
+
 	for i, (feat, labels) in enumerate(multi_label_feats):
 		for label in labels:
 			refsets[label].add(i)
-		
+
 		for label in multi_classifier.classify(feat):
 			testsets[label].add(i)
-	
+
 	return refsets, testsets
 
 def avg_masi_distance(multi_classifier, multi_label_feats):
 	mds = []
-	
+
 	for feat, labels in multi_label_feats:
 		mds.append(masi_distance(labels, multi_classifier.classify(feat)))
-	
+
 	if mds:
 		return float(sum(mds)) / len(mds)
 	else:
@@ -72,36 +72,39 @@ def cross_fold(instances, trainf, testf, folds=10, trace=1, metrics=True, inform
 	random.shuffle(instances)
 	l = len(instances)
 	step = l / folds
-	
+
 	if trace:
 		print 'step %d over %d folds of %d instances' % (step, folds, l)
-	
+
 	accuracies = []
 	precisions = collections.defaultdict(list)
 	recalls = collections.defaultdict(list)
 	f_measures = collections.defaultdict(list)
-	
+
 	for f in range(folds):
 		if trace:
 			print '\nfold %d' % (f+1)
 			print '-----%s' % ('-'*len('%s' % (f+1)))
-		
+
 		start = f * step
 		end = start + step
 		train_instances = instances[:start] + instances[end:]
 		test_instances = instances[start:end]
-		
+
 		if trace:
 			print 'training on %d:%d + %d:%d' % (0, start, end, l)
-		
+
 		obj = trainf(train_instances)
-		
+
 		if trace:
 			print 'testing on %d:%d' % (start, end)
-		
+
 		if metrics:
 			refsets, testsets = ref_test_sets(obj, test_instances)
-			
+
+			if trace:
+				print 'label, precision, recall, f-measure'
+
 			for key in set(refsets.keys() + testsets.keys()):
 				ref = refsets[key]
 				test = testsets[key]
@@ -111,38 +114,36 @@ def cross_fold(instances, trainf, testf, folds=10, trace=1, metrics=True, inform
 				precisions[key].append(p)
 				recalls[key].append(r)
 				f_measures[key].append(f)
-				
+
 				if trace:
-					print '%s precision: %f' % (key, p)
-					print '%s recall: %f' % (key, r)
-					print '%s f-measure: %f' % (key, f)
-		
+					print '%s, %f, %f, %f' % (key, p, r, f)
+
 		accuracy = testf(obj, test_instances)
-		
+
 		if trace:
 			print 'accuracy: %f' % accuracy
-		
+
 		accuracies.append(accuracy)
-		
+
 		if trace and informative and hasattr(obj, 'show_most_informative_features'):
 			obj.show_most_informative_features(informative)
-	
+
 	if trace:
 		print '\nmean and variance across folds'
 		print '------------------------------'
 		print 'accuracy mean: %f' % (sum(accuracies) / folds)
 		print 'accuracy variance: %f' % array(accuracies).var()
-		
+
 		for key, ps in precisions.iteritems():
 			print '%s precision mean: %f' % (key, sum(ps) / folds)
 			print '%s precision variance: %f' % (key, array(ps).var())
-		
+
 		for key, rs in recalls.iteritems():
 			print '%s recall mean: %f' % (key, sum(rs) / folds)
 			print '%s recall variance: %f' % (key, array(rs).var())
-		
+
 		for key, fs in f_measures.iteritems():
 			print '%s f_measure mean: %f' % (key, sum(fs) / folds)
 			print '%s f_measure variance: %f' % (key, array(fs).var())
-	
+
 	return accuracies, precisions, recalls, f_measures
